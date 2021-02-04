946dfd651f5fa7cd30f8d72c7efac41509a114a7
Huu Nhan <huunhankirigamer@gmail.com>
2021-02-05 01:26:25 +0700

    Create config, controllers, middlewares, models, routes with luv :heart:

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://happy-tea-1a89b-default-rtdb.firebaseio.com",
+});
+
+module.exports = admin;
