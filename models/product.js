51cdc957e040a31d77c4fbafdef4e916dbc17a79
Huu Nhan <huunhankirigamer@gmail.com>
2021-02-11 23:27:01 +0700

    Create controller, model, route for product with luv :heart:

---
+++ models/product.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    desciption: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    subs: [{ type: ObjectId, ref: "Sub" }],
+    quantity: Number,
+    sold: { type: Number, default: 0 },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    topping: {
+      type: String,
+      enum: [
+        "Trân châu đen",
+        "Trân châu ngọc trai",
+        "Thạch nha đam",
+        "Thạch trái cây",
+        "Kem cheese",
+        "Full topping",
+      ],
+    },
+    brand: {
+      type: String,
+      enum: ["Milksha", "Sharetea", "Coco", "Koi", "Chachago"],
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: {type: ObjectId, ref: "User"}
+    //   }
+    // ]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
