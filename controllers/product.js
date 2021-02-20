e6901442d8d56a02ef626da8a6e01c4187e6d95c
Huu Nhan <huunhankirigamer@gmail.com>
2021-02-20 22:22:55 +0700

    Modified controller, route product for pagination with luv :heart:

--- controllers/product.js
+++ controllers/product.js
@@ -66,3 +66,49 @@ exports.update = async (req, res) => {
     });
   }
 };
+
+// WITHOUT PAGINATION
+// exports.list = async (req, res) => {
+//   try {
+//     // createAt/updatedAt, desc/asc
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// WITH PAGINATION
+exports.list = async (req, res) => {
+  // console.table(req.body);
+  try {
+    // createAt/updatedAt, desc/asc
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 4;
+
+    const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
+      .populate("category")
+      .populate("subs")
+      .sort([[sort, order]])
+      .limit(perPage)
+      .exec();
+
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.productsCount = async (req, res) => {
+  let total = await Product.find({}).estimatedDocumentCount().exec();
+  res.json(total);
+};
