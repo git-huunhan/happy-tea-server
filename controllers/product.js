6a2c4d90b392e231a527f41f9f40bcc39274efb2
Huu Nhan <huunhankirigamer@gmail.com>
2021-04-13 23:56:40 +0700

    Complete controller product for sub tag with luv :heart:

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const User = require("../models/user");
 const slugify = require("slugify");
+const { aggregate } = require("../models/product");
 
 exports.create = async (req, res) => {
   try {
@@ -211,8 +212,45 @@ const handleCategory = async (req, res, category) => {
   }
 };
 
+const handleStar = (req, res, stars) => {
+  Product.aggregate([
+    {
+      $project: {
+        document: "$$ROOT",
+        // title: "$title",
+        floorAverage: {
+          $floor: { $avg: "$ratings.star" },
+        },
+      },
+    },
+    { $match: { floorAverage: stars } },
+  ])
+    .limit(12)
+    .exec((err, aggregates) => {
+      if (err) console.log("AGGREGATE ERROR", err);
+      Product.find({ _id: aggregates })
+        .populate("category", "_id name")
+        .populate("subs", "_id name")
+        .populate("postedBy", "_id name")
+        .exec((err, products) => {
+          if (err) console.log("PRODUCT AGGREGATE ERROR", err);
+          res.json(products);
+        });
+    });
+};
+
+const handleSub = async (req, res, sub) => {
+  const products = await Product.find({ subs: sub })
+    .populate("category", "_id name")
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+
+  res.json(products);
+};
+
 exports.searchFilters = async (req, res) => {
-  const { query, price, category } = req.body;
+  const { query, price, category, stars, sub } = req.body;
 
   if (query) {
     console.log("query", query);
@@ -228,5 +266,15 @@ exports.searchFilters = async (req, res) => {
   if (category) {
     console.log("category", category);
     await handleCategory(req, res, category);
+  }
+
+  if (stars) {
+    console.log("category", stars);
+    await handleStar(req, res, stars);
+  }
+
+  if (sub) {
+    console.log("sub", sub);
+    await handleSub(req, res, sub);
   }
 };
