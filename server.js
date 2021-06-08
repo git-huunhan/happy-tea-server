e44601e93b299b1b0b0c7b64713e5051c58b7152
Huu Nhan <huunhankirigamer@gmail.com>
2021-06-08 23:54:38 +0700

    Deploy test with luv :heart:

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const compression = require('compression');
+const path = require('path');
 require("dotenv").config();
 
 // app
@@ -21,9 +23,16 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERR", err));
 
 // middlewares
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
